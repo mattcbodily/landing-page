cc46e34b2bb9a4d7340f68f8ef879aad4058da15
mattcbodily <mattcbodily@gmail.com>
2024-07-25 21:29:06 -0600

    removes page-header component and adds the tool-bar component

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,13 @@
 <script setup>
-import PageHeader from './components/PageHeader.vue'
 import Clock from './components/widgets/Clock.vue'
 import TodoList from './components/widgets/TodoList.vue'
 import Pomodoro from './components/widgets/Pomodoro.vue'
+import ToolBar from './components/ToolBar.vue'
 </script>
 
 <template>
-  <PageHeader />
   <div
-    class="bg-slate-100 dark:bg-slate-800 min-h-screen px-4 pt-20 pb-4 grid grid-cols-4 sm:grid-cols-6 md:grid-cols-8 auto-rows-[75px] gap-2"
+    class="bg-slate-100 dark:bg-slate-800 min-h-screen p-4 grid grid-cols-4 sm:grid-cols-6 md:grid-cols-8 auto-rows-[75px] gap-2"
     @dragenter.prevent
     @dragover.prevent
     @drop="(event) => console.log('drop', event)"
@@ -16,5 +15,8 @@ import Pomodoro from './components/widgets/Pomodoro.vue'
     <Clock />
     <Pomodoro />
     <TodoList />
+  </div>
+  <div class="fixed bottom-4 w-full px-4">
+    <ToolBar />
   </div>
 </template>
