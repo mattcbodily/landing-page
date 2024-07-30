4e12202553864fe44177a6e519f060d68c30612b
mattcbodily <mattcbodily@gmail.com>
2024-07-30 10:23:48 -0600

    removes unused imports in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,4 @@
 <script setup>
-import Clock from './components/widgets/Clock.vue'
-import Pomodoro from './components/widgets/Pomodoro.vue'
-import TodoList from './components/widgets/TodoList.vue'
 import ToolBar from './components/ToolBar.vue'
 import { useWidgetStore } from './stores/widget'
 
@@ -9,7 +6,7 @@ const widgetStore = useWidgetStore()
 </script>
 
 <template>
-  <div class="min-h-screen p-4 bg-slate-100 dark:bg-slate-800 touch-none" id="widget-grid">
+  <div class="relative min-h-screen p-4 bg-slate-100 dark:bg-slate-800 touch-none" id="widget-grid">
     <template v-for="widget in widgetStore.widgets" :key="widget.uuid">
       <component :is="widget.component" :widget-data="widget"> </component>
     </template>
